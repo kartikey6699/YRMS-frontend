2aab21dc41a7c8355fd13896934236e7202a637c
Vikas  Tank <vikas.tank@Yash.com>
2025-03-12 19:40:11 +0530

    nav updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './componant/header/header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <h1 className='text-2xl'>Home</h1>
-    </ div>
+    <>
+    <Header />
+    </>
   )
 }
 
