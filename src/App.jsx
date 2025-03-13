9fa3473f9ed3b54ae4a0880d619f45061731e4a9
akhilesh <akhilesh.ingle@yash.com>
2025-03-13 12:17:57 +0530

    created folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router'
-import LandingPage from './componant/landingPage/LandingPage'
-import HeaderLayout from './componant/header/headerlayout'
+import LandingPage from './component/landingPage/LandingPage'
+import HeaderLayout from './component/header/headerlayout'
 
 function App() {
 
